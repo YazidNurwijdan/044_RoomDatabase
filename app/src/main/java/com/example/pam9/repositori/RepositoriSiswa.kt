57b9df23b93b3d6f9c89fd561b10eff5f2965222
YazidNurwijdan <ynurwijdan@gmail.com>
2025-11-28 23:52:17 +0700

    Menambah isi File RespositoriSiswa

--- app/src/main/java/com/example/pam9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pam9/repositori/RepositoriSiswa.kt
@@ -1,2 +1,19 @@
-package com.example.pam9.repositori
+package com.example.questroom.repositori
 
+import com.example.questroom.room.Siswa
+import com.example.questroom.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+) : RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+}
