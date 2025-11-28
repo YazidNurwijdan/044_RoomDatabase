6ed932b1078936188e60ad2ee0251848fae03c64
YazidNurwijdan <ynurwijdan@gmail.com>
2025-11-29 00:00:16 +0700

    Menambah isi File tambahan DatabaseSiswa

--- app/src/main/java/com/example/pam9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pam9/room/DatabaseSiswa.kt
@@ -8,3 +8,18 @@ import androidx.room.RoomDatabase
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase() {
     abstract fun siswaDao(): SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance :DatabaseSiswa ? = null
+
+        fun getDatabase (context: Context):DatabaseSiswa{
+            return Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    "db_siswa")
+                    .build().also { Instance = it }
+            }
+        }
+    }
+}
