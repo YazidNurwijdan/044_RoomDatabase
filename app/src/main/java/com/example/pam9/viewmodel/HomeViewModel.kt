e6419d6f00a226c5f208948aad6852eaef620040
YazidNurwijdan <ynurwijdan@gmail.com>
2025-11-29 00:16:36 +0700

    menambah isi file HomeViewModel

--- app/src/main/java/com/example/pam9/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pam9/viewmodel/HomeViewModel.kt
@@ -1,2 +1,31 @@
-package com.example.pam9.viewmodel
+package com.example.questroom.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questroom.repositori.RepositoriSiswa
+import com.example.questroom.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
+{
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState())
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+
+}
