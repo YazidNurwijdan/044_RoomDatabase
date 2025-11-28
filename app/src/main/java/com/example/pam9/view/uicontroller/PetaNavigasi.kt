fd65e4cb92c4197584db1fc94510cb6695d612fb
YazidNurwijdan <ynurwijdan@gmail.com>
2025-11-29 00:08:11 +0700

    menambah isi file PetaNavigasi

--- app/src/main/java/com/example/pam9/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/pam9/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,38 @@
-package com.example.pam9.view.uicontroller
+package com.example.questroom.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questroom.view.EntrySiswaScreen
+import com.example.questroom.view.HomeScreen
+import com.example.questroom.view.route.DestinasiEntry
+import com.example.questroom.view.route.DestinasiHome
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(),
+             modifier: Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
