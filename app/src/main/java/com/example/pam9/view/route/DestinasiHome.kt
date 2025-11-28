3e65bee77d721d0a070126e1dd3e9e8d597e386e
YazidNurwijdan <ynurwijdan@gmail.com>
2025-11-29 00:05:38 +0700

    menambah isi file DestinasiHome

--- app/src/main/java/com/example/pam9/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/pam9/view/route/DestinasiHome.kt
@@ -1,2 +1,8 @@
 package com.example.questroom.view.route
 
+import com.example.questroom.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
