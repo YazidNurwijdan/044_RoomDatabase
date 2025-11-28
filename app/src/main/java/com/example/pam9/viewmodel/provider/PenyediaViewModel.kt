4da86e5f4240954f43aebe2fdb3cbf0399afc3dd
YazidNurwijdan <ynurwijdan@gmail.com>
2025-11-29 00:11:26 +0700

    menambah isi file penyediaviewmodel

--- app/src/main/java/com/example/pam9/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam9/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,26 @@
-package com.example.pam9.viewmodel.provider
+package com.example.questroom.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questroom.repositori.AplikasiSiswa
+import com.example.questroom.viewmodel.EntryViewModel
+import com.example.questroom.viewmodel.HomeViewModel
+
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiSiswa)
